f47f7260874a8f5451b96885928f1488f3e80ebb
quannguyenhai2001 <nguyenhaiquan.0121@gmail.com>
2024-03-15 17:29:00 +0700

    feat: add multi language feature

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { I18nextProvider } from 'react-i18next'
-import i18n from './locales/i18n';
+import i18n from './locales/i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
